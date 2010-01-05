ba13c8f5c1531093e387341ccf738f0c059c5ae4
Jesper Kristensen <cableman@portable.(none)>
2010-01-05 10:42:07 +0100

    Moved script to footer

--- issuu.js
+++ issuu.js
@@ -1,3 +1,1 @@
-$(document).ready(function () {
-  document.write(unescape("%3Cscript src='http://static.issuu.com/smartlook/ISSUU.smartlook.js' type='text/javascript'%3E%3C/script%3E"));
-});
+document.write(unescape("%3Cscript src='http://static.issuu.com/smartlook/ISSUU.smartlook.js' type='text/javascript'%3E%3C/script%3E"));
